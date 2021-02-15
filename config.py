2d5e5b8bbb87739d05a58079d4a8c029c23ed71b
Lucas Almeida <72702201+tekukas@users.noreply.github.com>
2021-02-15 12:05:29 -0300

    mudanca na escala da tela

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 black = (0, 0, 0)
 white = (255, 255, 255)
 
-scale = 32
+scale = 64  # 32
 
-screen_height = 480
-screen_width = 640
+screen_height = int(480 * 1.4)
+screen_width = int((screen_height * 4/3) * 1.4)
