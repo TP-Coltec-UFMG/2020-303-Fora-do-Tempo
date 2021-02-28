54b26456c509a034c6852e32d02ca500b00fd122
sammy205 <73919775+sammy205@users.noreply.github.com>
2021-02-28 18:56:42 -0300

    Adição do NPC

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import pygame
 import config
 from game import Game
 from player import Player
+from npc import Npc
 
 # inicializando a biblioteca
 pygame.init()
 
 # inicializando a tela
 screen = pygame.display.set_mode((config.screen_width, config.screen_height), 0)
+TILE_SIZE = 128
+
 # fonte = pygame.font.SysFont("arial", 24, True, False)
 
 pygame.display.set_caption("Fora de Tempo")
@@ -17,6 +20,7 @@ clock = pygame.time.Clock()
 if __name__ == "__main__":
     player = Player(1, 1, screen)
     game = Game(player, screen)
+    npc = Npc()
 
     while True:
         clock.tick(10)
@@ -31,8 +35,11 @@ if __name__ == "__main__":
         game.pintar(game.camera)
         player.pintar(game.camera)
         pygame.display.update()
+        npc.update(player)
+        npc.draw(screen)
 
         # Captura de eventos
         eventos = pygame.event.get()
         game.processar_eventos(eventos)
         player.processar_eventos(eventos)
+        
